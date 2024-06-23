f9da6a5f48fe3ae90ff969a1561e1de69e5af836
wood3n <wangkka1@163.com>
2024-06-23 21:43:46 +0800

    chore(all): test publish tools

--- README.md
+++ README.md
@@ -1,17 +1,5 @@
 # pnpm-monorepo-template
-frontend monorepo template based on pnpm.
 
-## eslint config
+pnpm workspace usage.
 
-```shell
-@babel/eslint-parser 
-@babel/core 
-@babel/eslint-plugin
-eslint
-eslint-config-airbnb-config
-eslint-plugin-import 
-eslint-plugin-jsx-a11y
-@typescript-eslint/parser 
-@typescript-eslint/eslint-plugin 
-eslint-import-resolver-typescript
-```
+[![semantic-release: angular](https://img.shields.io/badge/semantic--release-angular-e10079?logo=semantic-release)](https://github.com/semantic-release/semantic-release)
---
+++ packages/app1/release.config.js
@@ -0,0 +1,25 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+  branches: "master",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: ["dist/**"],
+      },
+    ],
+  ],
+};
